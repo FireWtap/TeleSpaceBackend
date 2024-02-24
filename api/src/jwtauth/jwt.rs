11b6eb8dbe4efba84cabe52c3c6b5236e100eae9
francesco <massafra32@gmail.com>
2024-02-24 15:11:09 +0100

    confused stuff

---
+++ api/src/jwtauth/jwt.rs
@@ -0,0 +1,53 @@
+use std::env;
+use chrono::Utc;
+use rocket::serde::{Deserialize, Serialize};
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Claims {
+    pub subject_id: i32,
+    exp: usize,
+}
+
+#[derive(Debug)]
+pub struct JWT {
+    pub claims: Claims,
+}
+
+
+
+use jsonwebtoken::errors::{Error, ErrorKind};
+use jsonwebtoken::{Algorithm, decode, DecodingKey, encode, EncodingKey, Header, Validation};
+
+pub fn create_jwt(id: i32) -> Result<String, Error> {
+    let secret = env::var("JWT_SECRET").expect("JWT_SECRET must be set."); // 👈 New!
+
+    let expiration = Utc::now()
+        .checked_add_signed(chrono::Duration::seconds(60))
+        .expect("Invalid timestamp")
+        .timestamp();
+
+    // 👇 New!
+    let claims = Claims {
+        subject_id: id,
+        exp: expiration as usize,
+    };
+
+    // 👇 New!
+    let header = Header::new(Algorithm::HS512);
+
+    encode(&header, &claims, &EncodingKey::from_secret(secret.as_bytes()))
+}
+
+
+pub fn decode_jwt(token: String) -> Result<Claims, ErrorKind> {
+    let secret = env::var("JWT_SECRET").expect("JWT_SECRET must be set.");
+    let token = token.trim_start_matches("Bearer").trim();
+    match decode(
+        &token,
+        &DecodingKey::from_secret(secret.as_bytes()),
+        &Validation::new(Algorithm::HS512),
+    ) {
+        Ok(token) => Ok(token.claims),
+        Err(err) => Err(err.kind().to_owned())
+    }
+}
